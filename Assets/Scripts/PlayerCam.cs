1e2e4749f8569acf9f17a19bce323fd7ce46ebf6
Paez11 <victorpaez117@gmail.com>
2023-09-15 14:20:56 +0200

    cameras style

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -9,6 +9,11 @@ public class PlayerCam : MonoBehaviour
     public Transform playerObj;
     public float rotationSpeed;
 
+    CameraStyle currentStyle;
+
+    public GameObject topDownCam;
+    public GameObject basicCam;
+
     private void Start() 
     {
         Cursor.lockState = CursorLockMode.Confined;
@@ -23,11 +28,33 @@ public class PlayerCam : MonoBehaviour
         orientation.forward = viewDir.normalized;
 
         // roate player object
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        if(currentStyle == CameraStyle.Topdown)
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        if(inputDir != Vector3.zero)
-            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+            if(inputDir != Vector3.zero)
+                playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+        }
+
+        if(currentStyle == CameraStyle.Basic)
+        {
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.I)) SwitchCameraStyle(CameraStyle.Basic);
+        if(Input.GetKeyDown(KeyCode.Escape)) SwitchCameraStyle(CameraStyle.Topdown);
+    }
+
+    public void SwitchCameraStyle(CameraStyle newStyle)
+    {
+        topDownCam.SetActive(false);
+        basicCam.SetActive(false);
+
+        if(newStyle == CameraStyle.Basic) basicCam.SetActive(true);
+        if(newStyle == CameraStyle.Topdown) topDownCam.SetActive(true);
+
+        currentStyle = newStyle;
     }
 }
